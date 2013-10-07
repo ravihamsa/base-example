8dfa5365977074e690c045b4eabb9f4b2e6698dc
Ravi Kumar Hamsa <ravi.hamsa@inmobi.com>
2013-10-07 18:29:13 +0530

    filter and pagination added to table

--- js/apps/examples/pages/table.js
+++ js/apps/examples/pages/table.js
@@ -8,15 +8,16 @@
 
 define(['base', 'widgets/table', './examplePage'], function (Base, Table, ExamplePage) {
 
-    var app = Base.app;
+    var baseApp = Base.app;
 
 
     var PageView = ExamplePage.View.extend({
         examples: [
             {
-                func: simpleTable,
-                title: 'Simple Table'
+                func:extendedTable,
+                title:'Extended Table'
             }
+
         ]
     })
 
@@ -24,7 +25,7 @@ define(['base', 'widgets/table', './examplePage'], function (Base, Table, Exampl
     function simpleTable(previewEl, consoleEl) {
         //starts here
 
-        var arr = getTableData();
+        var arr = getTableData(123);
 
         var columns = _.map(_.keys(arr[0]),function(key){
             var obj= {key:key}
@@ -32,19 +33,32 @@ define(['base', 'widgets/table', './examplePage'], function (Base, Table, Exampl
                 obj.formatter = 'currency'
                 obj.align = 'right';
             }
-            return obj;
 
+            if(key==='invoice'){
+                obj.formatter = 'invoiceLink'
+                obj.renderHTML = true;
+            }
+            return obj;
         });
 
+        baseApp.setFormatter('invoiceLink', function(value, obj){
+            return '<a href="#download/'+obj.transaction_id+'" class="action">download</a>';
+        })
+
+
+
         var coll = new Table.RowCollection(arr);
+        coll.setConfigs({paginated:true, page:1, perPage:25});
+        coll.addFilter({column: 'mode', expr: 'startsWith', value: 'pay'})
 
         var tableModel = new Table.Model();
-        tableModel.setConfig('sortKey', 'name');
+        coll.setConfig('sortKey', 'date');
 
         var view = new Table.View({
             columns: columns,
             rowCollection: coll,
-            model: tableModel
+            model: tableModel,
+            template: '<div class="table-header"></div> <table class="row-list"></table><div class="table-footer"> <a href="#nextPage" class="action">Next Page</a> <a href="#prevPage" class="action">Prev Page</a></div>'
         })
 
         view.render();
@@ -55,7 +69,77 @@ define(['base', 'widgets/table', './examplePage'], function (Base, Table, Exampl
     }
 
 
-    function getTableData (){
+    function extendedTable(previewEl, consoleEl) {
+        //starts here
+
+        var arr = getTableData(123);
+
+        var columns = _.map(_.keys(arr[0]),function(key){
+            var obj= {key:key}
+            if(key==='amount'){
+                obj.formatter = 'currency'
+                obj.align = 'right';
+            }
+
+            if(key==='invoice'){
+                obj.formatter = 'invoiceLink'
+                obj.renderHTML = true;
+            }
+            return obj;
+        });
+
+        baseApp.setFormatter('invoiceLink', function(value, obj){
+            return '<a href="#download/'+obj.transaction_id+'" class="action">download</a>';
+        })
+
+
+
+        var coll = new Table.RowCollection(arr, {config:{paginated:true, page:1, perPage:5}});
+
+        //coll.addFilter({column: 'mode', expr: 'startsWith', value: 'pay'})
+
+        var tableModel = new Table.Model();
+        coll.setConfig('sortKey', 'date');
+
+
+        var ExtendedTable = Table.View.extend({
+            template: '<div class="table-header"></div> <table class="row-list"></table><div class="table-footer"> <a href="#nextPage" class="action">Next Page</a> <a href="#prevPage" class="action">Prev Page</a></div>',
+            actionHandler:function(action){
+                var coll = this.getOption('rowCollection');
+                switch(action){
+                    case 'nextPage':
+                        coll.nextPage();
+                        break;
+                    case 'prevPage':
+                        coll.prevPage();
+                        break;
+                }
+                console.log(action);
+            }
+        })
+
+
+        var view = new ExtendedTable({
+            columns: columns,
+            rowCollection: coll,
+            model: tableModel
+
+        })
+
+
+        coll.on('config_change',function(){
+            view.render();
+        })
+
+        view.render();
+
+        //ends
+
+        view.$el.appendTo(previewEl);
+    }
+
+
+    function getTableData (recordCount){
         var modeOptions = ['From Earnings',
             'Credit Card',
             'PromoCode',
@@ -70,7 +154,7 @@ define(['base', 'widgets/table', './examplePage'], function (Base, Table, Exampl
             return arr[Math.floor(Math.random()*arr.length)]
         }
 
-        var arr = _.range(0,10).map(function(index){
+        var arr = _.range(0,recordCount).map(function(index){
 
 
             return {
@@ -79,7 +163,8 @@ define(['base', 'widgets/table', './examplePage'], function (Base, Table, Exampl
                 amount:getRandomItem(amountOptions),
                 transaction_id:hex_md5(new Date().getTime()),
                 comment:getRandomItem(commentOptions),
-                invoice:''
+                invoice:'',
+                index:index
             }
         })
 
